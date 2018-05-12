35752c58e9373e12bb77ee07f45b597ceaf821bf
petrox <petrox.varga@gmail.com>
2018-05-12 10:43:44 +0200

    lcd changes

--- lcd/lcd.h
+++ lcd/lcd.h
@@ -1,15 +1,8 @@
 #ifndef LCD_H_INCLUDED
 #define LCD_H_INCLUDED
 
-#define RS PF1
-#define RW PF2
-#define ENABLE PF3
 #define DATA PORTE
 #define CTRL PORTF
-#define DB7 PE7
-#define DB6 PE6
-#define DB5 PE5
-#define DB4 PE4
 
 void LCD_init(void);
 void LCD_sendEnable(void);
@@ -18,4 +11,5 @@ void LCD_sendData(unsigned char data);
 void LCD_sendString(unsigned char *str);
 void LCD_setCursor(unsigned char row, unsigned char pos);
 void LCD_clearScreen(void);
+void LCD_isBusy(void);
 #endif
