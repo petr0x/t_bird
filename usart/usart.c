8463f317a5fcff1c3b53179a64617494b96a02f4
petr0x <petrox.varga@gmail.com>
2018-05-17 12:41:07 +0200

    Update usart.c

--- usart/usart.c
+++ usart/usart.c
@@ -1,10 +1,10 @@
 
-#define F_CPU 8000000UL
+#include "tbird.h"
+#include "usart.h"
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include "usart.h"
 
 void USART_init(unsigned int baud){
 
@@ -13,8 +13,8 @@ void USART_init(unsigned int baud){
 	UBRR1H = (baud>>8); //Set baud Rate
 	UBRR1L = baud;
 	
-	UCSR1B = (1<<RXEN1)|(1<<TXEN1)|(1<<RXCIE1); 
-	UCSR1C = (1<<USBS1)|(1<<UCSZ11)|(1<<UCSZ10);
+	UCSR1B = (1 << RXEN1) | (1 << TXEN1) | (1 << RXCIE1);   //RX enable // TX enable // RX interrupt enable
+	UCSR1C = (1 << USBS1) | (1 << UCSZ11) | (1 << UCSZ10);  // 1 start 1 stop 8 data bit
 }
 
 void USART_transmit(unsigned char data)
